72ebb7e8af2f365ca4e1cde18680a51c7b6f41ae
jeydybet <1008220231@unajma.edu.pe>
2025-11-22 23:19:39 -0500

    Primer commit del proyecto Angular + Firebase

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,26 @@
 import { bootstrapApplication } from '@angular/platform-browser';
-import { appConfig } from './app/app.config';
 import { App } from './app/app';
 
-bootstrapApplication(App, appConfig)
-  .catch((err) => console.error(err));
+// Firebase / AngularFire
+import { provideFirebaseApp, initializeApp } from '@angular/fire/app';
+import { provideAnalytics, getAnalytics, ScreenTrackingService, UserTrackingService } from '@angular/fire/analytics';
+
+// Config Firebase
+const firebaseConfig = {
+  apiKey: "AIzaSyAoQMk4S0K5Jb52Craq28fMqLVhQvuAcqg",
+  authDomain: "gestor-proyectos-personales.firebaseapp.com",
+  projectId: "gestor-proyectos-personales",
+  storageBucket: "gestor-proyectos-personales.firebasestorage.app",
+  messagingSenderId: "944199391992",
+  appId: "1:944199391992:web:bd7462cbd8e03431adb90d",
+  measurementId: "G-VD4MJCRJ14"
+};
+
+bootstrapApplication(App, {
+  providers: [
+    provideFirebaseApp(() => initializeApp(firebaseConfig)),
+    provideAnalytics(() => getAnalytics()),
+    ScreenTrackingService,
+    UserTrackingService
+  ],
+});
